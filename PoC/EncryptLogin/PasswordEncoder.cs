7ead518f8bd23f1a6fa37f015c6839064ba70164
hlam3004 <hlam.30042005@gmail.com>
2026-03-09 21:38:35 +0700

    feat(PoC): implement encrypt login

---
+++ PoC/EncryptLogin/PasswordEncoder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptLogin
+{
+    public class PasswordEncoder
+    {
+
+        public string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                StringBuilder builder = new StringBuilder();
+
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+
+        public bool VerifyPassword(string inputPassword, string storedHash)
+        {
+            string hashedInput = HashPassword(inputPassword);
+            return hashedInput == storedHash;
+        }
+    }
+}
