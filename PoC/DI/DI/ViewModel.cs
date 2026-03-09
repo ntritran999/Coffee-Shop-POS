b18d2d81dad6527aafc81a7354fb50bd70033970
ntritran999 <ntritran999@gmail.com>
2026-03-09 10:06:25 +0700

    Add DI PoC

---
+++ PoC/DI/DI/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using DI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DI
+{
+    public class ViewModel
+    {
+        public Model MyModel { get; set; }
+        private IMessageService _messageService;
+
+        public ViewModel(IMessageService messageService) {
+            _messageService = messageService;
+
+            MyModel = new Model
+            {
+                Name = "Alice",
+            };
+        }
+
+        public String ShowMessage(String Name)
+        {
+            return _messageService.ShowMessage(Name);
+        }
+    }
+}
