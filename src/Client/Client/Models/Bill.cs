4b09b76a5b080f0de24768645898a2c016154fb5
nghiep2405 <nghiep2405@gmail.com>
2026-03-18 13:35:09 +0700

    feat: create model for app

---
+++ src/Client/Client/Models/Bill.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client.Models
+{
+    public class Bill
+    {
+        public int BillID { get; set; }
+        public DateTime DateCheckIn { get; set; }
+
+        // Dùng DateTime? vì có thể hóa đơn chưa thanh toán nên chưa có giờ ra
+        public DateTime? DateCheckOut { get; set; }
+
+        // Dùng int? vì cho phép Null nếu khách mua mang về
+        public int? TableID { get; set; }
+
+        public int Status { get; set; } // 0: Chưa thanh toán, 1: Đã thanh toán
+        public double TotalAmount { get; set; }
+        public double Discount { get; set; }
+    }
+}
---
+++ src/Client/Client/Models/Table.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client.Models
+{
+    public class Table
+    {
+        public int TableID { get; set; }
+        public string TableName { get; set; }
+        public int Status { get; set; } // 0: Trống, 1: Có người, 2: Đang dọn dẹp
+    }
+}
