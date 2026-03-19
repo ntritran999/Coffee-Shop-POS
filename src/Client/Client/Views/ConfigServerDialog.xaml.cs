4be6e416ce584e661be9de57553dd44f5500a8af
hlam3004 <hlam.30042005@gmail.com>
2026-03-19 23:21:21 +0700

    feat: add config server dialog

--- src/Client/Client/Views/ConfigServerDialog.xaml.cs
+++ src/Client/Client/Views/ConfigServerDialog.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
@@ -21,24 +21,11 @@ namespace Client.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class LoginPage : Page
+    public sealed partial class ConfigServerDialog : ContentDialog
     {
-        public LoginPage()
+        public ConfigServerDialog()
         {
             InitializeComponent();
-        }
-
-        private async void ConfigServer_Click(object sender, RoutedEventArgs e)
-        {
-            var configServerDialog = new ConfigServerDialog();
-            configServerDialog.XamlRoot = this.XamlRoot;
-            configServerDialog.Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style;
-            configServerDialog.PrimaryButtonText = "Lưu cấu hình";
-            configServerDialog.CloseButtonText = "Hủy";
-            configServerDialog.DefaultButton = ContentDialogButton.Primary;
-
-
-            var result = await configServerDialog.ShowAsync();
         }
     }
 }
