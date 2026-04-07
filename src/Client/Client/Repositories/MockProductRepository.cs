3d884fb430f1d4ebcb62f3d0f525dc41e1e32ead
isQHung <testkiller011@gmail.com>
2026-04-07 17:26:21 +0700

    feat: add product client.

--- src/Client/Client/Repositories/MockProductRepository.cs
+++ src/Client/Client/Repositories/MockProductRepository.cs
@@ -1,6 +1,8 @@
 ﻿using Client.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,45 +10,71 @@ namespace Client.Repositories
 {
     public class MockProductRepoitory : IProductRepository
     {
+        static public List<Product> _products = new List<Product>() {
+            new Product() { ProductID = 1, Name = "Product 1", Price = 10000, Image = "/Assets/ProductSource/1.png", CategoryID = 1 },
+            new Product() { ProductID = 2, Name = "Product 2", Price = 45000, Image = "/Assets/ProductSource/2.png", CategoryID = 1 },
+            new Product() { ProductID = 3, Name = "Product 3", Price = 55000, Image = "/Assets/ProductSource/3.png", CategoryID = 2 },
+            new Product() { ProductID = 4, Name = "Product 4", Price = 20000, Image = "/Assets/ProductSource/4.png", CategoryID = 2 },
+            new Product() { ProductID = 5, Name = "Product 5", Price = 32000, Image = "/Assets/ProductSource/5.png", CategoryID = 3 },
+        };
+
+        public MockProductRepoitory() { }
         public Task<Product> Add(Product item)
         {
-            throw new NotImplementedException();
+            int newId = _products.Last().ProductID + 1;
+
+            item.ProductID = newId;
+            _products.Add(item);
+
+            Debug.WriteLine("INFOR: ADD PRODUCT - MOCK");
+            Debug.WriteLine(_products.Count());
+
+            return Task.FromResult(item);
         }
 
         public Task<bool> Delete(string id)
         {
-            throw new NotImplementedException();
+            var p = _products.FirstOrDefault(p => p.ProductID.ToString() == id);
+
+            if (p == null) return Task.FromResult(false);
+            
+            _products.Remove(p);
+
+            return Task.FromResult(true);
         }
 
         public Task<IEnumerable<Product>> GetAll()
         {
-            return Task.FromResult<IEnumerable<Product>>([
-                new Product() { ProductID = 1, Name = "Product 1", Price = 10000, Image = "/Assets/ProductSource/1.png", CategoryID = 1 },
-                new Product() { ProductID = 2, Name = "Product 2", Price = 45000, Image = "/Assets/ProductSource/2.png", CategoryID = 1 },
-                new Product() { ProductID = 3, Name = "Product 3", Price = 55000, Image = "/Assets/ProductSource/3.png", CategoryID = 2 },
-                new Product() { ProductID = 4, Name = "Product 4", Price = 20000, Image = "/Assets/ProductSource/4.png", CategoryID = 2 },
-                new Product() { ProductID = 5, Name = "Product 5", Price = 32000, Image = "/Assets/ProductSource/5.png", CategoryID = 3 },
-            ]);
+            return Task.FromResult<IEnumerable<Product>>(_products);
         }
 
         public Task<IEnumerable<Product>> GetByCategory(string categoryId)
         {
-            throw new NotImplementedException();
+            return Task.FromResult<IEnumerable<Product>>(_products.Where(p => p.CategoryID.ToString() == categoryId));
         }
 
         public Task<Product?> GetById(string itemId)
         {
-            throw new NotImplementedException();
+            return Task.FromResult<Product?>(_products.FirstOrDefault(p => p.ProductID.ToString() == itemId));
         }
 
         public Task<IEnumerable<Product>> GetByName(string name)
         {
-            throw new NotImplementedException();
+            return Task.FromResult<IEnumerable<Product>>(_products.Where(p => p.Name.ToLower().Contains(name.ToLower())));
         }
 
         public Task<bool> Update(Product item)
         {
-            throw new NotImplementedException();
+            var p = _products.FirstOrDefault(p => p.ProductID == item.ProductID);
+            
+            if (p == null) return Task.FromResult(false);
+
+            p.Name = item.Name;
+            p.Price = item.Price;
+            p.Unit = item.Unit;
+            p.CategoryID = item.CategoryID;
+
+            return Task.FromResult(true);
         }
     }
 }
--- src/Client/Client/Services/ProductService.cs
+++ src/Client/Client/Services/ProductService.cs
@@ -18,5 +18,36 @@ namespace Client.Services
             return await _product.GetAll();
         }
 
+        public async Task<Product?> GetProductByID(int id)
+        {
+            string idString = id.ToString();
+            return await _product.GetById(idString);
+
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByCategory(string categoryId)
+        {
+            return await _product.GetByCategory(categoryId);
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByName(string name)
+        {
+            return await _product.GetByName(name);
+        }
+
+        public async Task<Product> AddProduct(Product item)
+        {
+            return await _product.Add(item);
+        }
+
+        public async Task<bool> UpdateProduct(Product item)
+        {
+            return await _product.Update(item);
+        }
+
+        public async Task<bool> DeleteProduct(int id)
+        {
+            return await _product.Delete(id.ToString());
+        }
     }
 }
