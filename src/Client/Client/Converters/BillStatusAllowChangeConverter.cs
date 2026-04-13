ab974c414d6a7651de5a88046efc97470f41da1d
ntritran999 <ntritran999@gmail.com>
2026-04-14 01:41:08 +0700

    fix: Disable pay and cancel bill after payment/cancellation. Convert datetime to local after fetching data.

---
+++ src/Client/Client/Converters/BillStatusAllowChangeConverter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client.Converters
+{
+    public class BillStatusAllowChangeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (value is int status && status == 0) ? true : false; 
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
